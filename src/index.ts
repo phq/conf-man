222d1aef7c8ad6fb23eb243f3f4898fe48cfd067
philaw <phq@users.noreply.github.com>
2018-08-27 12:34:32 +1000

    Enable node types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello TypeScript!');
+import fs from 'fs';
+
+console.log('Hello!');
+
+interface IOptions {
+    defaultConfigPath?: string;
+    localConfigPath?: string;
+}
+const options: IOptions = {};
+const defaultConfigPath = options.defaultConfigPath || 'config.json';
+const localConfigPath = options.localConfigPath || 'config.local.json';
+
+// read config
+let defaultConfig = fs.existsSync(defaultConfigPath) ? fs.readFileSync(defaultConfigPath, 'utf8') : {};
+let localConfig = fs.existsSync(localConfigPath) ? fs.readFileSync(localConfigPath, 'utf8') : {};
+
+const resolvedConfig = Object.assign({}, defaultConfig, localConfig);
+
+console.log(`Resolved config: ${JSON.stringify(resolvedConfig)}`);
+console.log('Done!');
